b231dd319d2ba632b09577be3643bf5551fe3cc9
Stevtm <steventmaddison@outlook.com>
2021-06-29 23:07:53 -0400

    update server.js

--- server.js
+++ server.js
@@ -3,14 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3001;
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-	useFindAndModify: false,
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-});
-
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(logger("dev"));
 
@@ -20,10 +14,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
-	useNewUrlParser: true,
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 	useFindAndModify: false,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
 });
+
+// mongoose.connect(MONGODB_URI, {
+// 	useNewUrlParser: true,
+// 	useFindAndModify: false,
+// });
 
 // routes
 app.use(require("./routes/api.js"));
